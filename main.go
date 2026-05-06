14b5cd8dfb1c06708096651f99fa35b67910e457
unpublished3 <chapagainsakshyam@gmail.com>
2026-05-06 17:31:02 +0545

    Chapter 2.4

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +12,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from snippetView"))
+	id, err  := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 0 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Hello %d from snippedView", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -22,7 +29,7 @@ func main()  {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on: 4000")
