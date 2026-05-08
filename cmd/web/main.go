cfbf368178b4c4244ec9116cf19984f382a9cd24
unpublished3 <chapagainsakshyam@gmail.com>
2026-05-08 11:19:23 +0545

    Chapter 3.1

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
-func main()  {
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+type config struct {
+	addr string
+	staticDir string
+}
 
+func main()  {
+	var cfg config
+
+	// Command-line arguments
+	flag.StringVar(&cfg.addr ,"addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "staticDir", "./ui/static/", "Static directory path")
+	flag.Parse();
+
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
+
+	// Handlers
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home)
@@ -16,8 +30,10 @@ func main()  {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on: 4000")
+	// Logger
+	log.Print("starting server on: ", cfg.addr, " & static dir ", cfg.staticDir)
 
-	err := http.ListenAndServe(":4000", mux)
+	// Error handler
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
